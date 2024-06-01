97440f9a0995afa7729faf0495dd2d3b74d0adb1
Kristian Drucker <kristian.drucker@gmail.com>
2024-06-01 11:30:06 +0200

    install chrome

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ RELEASE="$(rpm -E %fedora)"
 # this installs a package from fedora repos
 rpm-ostree install screen qemu-guest-agent
 
-flatpak install flathub com.google.Chrome
+flatpak_install flathub com.google.Chrome
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
